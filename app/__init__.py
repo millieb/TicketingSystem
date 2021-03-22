b589a8a1aa25418fbf6c12b135a1a7d6b0c998a6
Mildred Brito <mildred.brito@outlook.com>
2021-03-21 22:56:26 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -29,9 +29,14 @@ def create_app(config):
     app.config.from_object(config)
 
     csrf.init_app(app)
-    bootstrap.init_app(app)
+
+    if not app.config.get('TEST', False):
+        bootstrap.init_app(app)
+
+    app.app_context().push()
+
     login_manager.init_app(app)
-    login_manager.login_view = '.login'
+    login_manager.login_view = '.login'# url_for
     login_manager.login_message = LOGIN_REQUIRED
 
     mail.init_app(app)
@@ -39,9 +44,7 @@ def create_app(config):
     app.register_blueprint(page)
 
     with app.app_context():
-        #Realizamos coneccion a base de datos mediante el contexto
         db.init_app(app)
-        #Creamos todas nuestras tablas
         db.create_all()
-    
-    return app 
+
+    return app
