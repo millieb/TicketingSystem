eabb9698a405e3934adb3d54d5bd6ef95cd3ae8a
Mildred Brito <mildred.brito@outlook.com>
2021-03-21 22:50:00 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+from decouple import config
+
 class Config:
     #Utilizar llave diference y alphanumeric
     SECRET_KEY = 'codigofacilito' 
@@ -10,10 +12,20 @@ class DevelopmentConfig(Config):
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'test.mbrito@gmail.com' 
-    MAIL_PASSWORD = 'TestMildredBrito123' #ALWAYS safer to create an environment variable
+    MAIL_USERNAME = config('MAIL_USERNAME', default='test.mbrito@gmail.com')
+    MAIL_PASSWORD = config('MAIL_USERNAME', default='TestMildredBrito123') #ALWAYS safer to create an environment variable
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/project_web_facilito_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TEST = True
+
+class ProductionConfig(DevelopmentConfig):
+    DEBUG = False
 
 config = {
     'development': DevelopmentConfig,
-    'default' : DevelopmentConfig
+    'default': DevelopmentConfig,
+    'test': TestConfig,
+    'production': ProductionConfig
 }
